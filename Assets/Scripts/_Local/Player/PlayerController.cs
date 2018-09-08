b6e3f3c47417ca4c8e9afce9e8dfb137d34a703b
CoheeStudio <alwayssmile11a1@gmail.com>
2018-09-08 17:05:56 +0700

    nothing much

--- Assets/Scripts/_Local/Player/PlayerController.cs
+++ Assets/Scripts/_Local/Player/PlayerController.cs
@@ -6,60 +6,129 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-    public LayerMask hangingCheckLayer;
+
+    [Header("Ground Check")]
     public LayerMask groundLayer;
-    public float castDistance = 0.2f;
+    public float groundCastDistance = 0.2f;
+
+    [Header("Hanging Check")]
+    public LayerMask hangingCheckLayer;
+    public float hangingCastDistance = 0.2f;
+
+    [Header("Jumping Check")]
+    public float jumpCastDistance = 0.5f;
+    public float jumpCastRadius = 0.3f;
+
 
     private Rigidbody2D m_Rigidbody2D;
     private CapsuleCollider2D m_CapsuleCollider2D;
-    private ContactFilter2D m_ContactFilter2D = new ContactFilter2D();
+    private Animator m_Animator;
+    private ContactFilter2D m_HangingCheckContactFilter2D = new ContactFilter2D();
+    private ContactFilter2D m_GroundCheckContactFilter2D = new ContactFilter2D();
     private RaycastHit2D[] m_HitResults = new RaycastHit2D[2];
-    private float m_MovedDistance;
 
 
+    private bool m_IsGrounded;
+    private bool m_IsHanging;
 
     private void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
         m_CapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        m_Animator = GetComponentInChildren<Animator>();
 
-        m_ContactFilter2D.useTriggers = true;
-        m_ContactFilter2D.useLayerMask = true;
-        m_ContactFilter2D.layerMask = hangingCheckLayer;
+        m_HangingCheckContactFilter2D.useTriggers = true;
+        m_HangingCheckContactFilter2D.useLayerMask = true;
+        m_HangingCheckContactFilter2D.layerMask = hangingCheckLayer;
+
+        m_GroundCheckContactFilter2D.useTriggers = false;
+        m_GroundCheckContactFilter2D.useLayerMask = true;
+        m_GroundCheckContactFilter2D.layerMask = groundLayer;
+
         Physics2D.queriesStartInColliders = false;
     }
 
     private void Update()
     {
+        CheckForGround();
+        
+        if(!m_IsHanging)
+        {
+            CheckForHangableObject();
+        }
+
         CheckForHanging();
     }
 
     private void FixedUpdate()
     {
         m_Rigidbody2D.velocity = new Vector2(speed, m_Rigidbody2D.velocity.y);
+        
+        //m_MovedDistance += speed * Time.deltaTime;
+    }
 
-        m_MovedDistance += speed * Time.deltaTime;
+    private void CheckForGround()
+    {
+        int hitCount = Physics2D.Raycast(transform.position + ((Vector3)m_CapsuleCollider2D.offset - Vector3.up * m_CapsuleCollider2D.size.y/2) * transform.lossyScale.y, 
+                                            Vector3.down, m_GroundCheckContactFilter2D, m_HitResults, groundCastDistance);
+
+        Debug.DrawRay(transform.position + ((Vector3)m_CapsuleCollider2D.offset - Vector3.up * m_CapsuleCollider2D.size.y / 2) * transform.lossyScale.y, 
+                                            Vector3.down * groundCastDistance);
+
+        if(hitCount>0)
+        {
+            m_IsGrounded = true;
+        }
+        else
+        {
+            m_IsGrounded = false;
+        }
+
+    }
+
+    private void CheckForHangableObject()
+    {
+        int hitCount = Physics2D.CircleCast(transform.position + ((Vector3)m_CapsuleCollider2D.offset + Vector3.up * m_CapsuleCollider2D.size.y / 2) * transform.lossyScale.y,
+                                        jumpCastRadius, Vector3.one, m_HangingCheckContactFilter2D, m_HitResults, jumpCastDistance);
+
+        Debug.DrawRay(transform.position + ((Vector3)m_CapsuleCollider2D.offset + Vector3.up * m_CapsuleCollider2D.size.y / 2) * transform.lossyScale.y,
+                        Vector3.one * jumpCastDistance);
+
+        if (hitCount > 0)
+        {
+            m_Rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+            m_Rigidbody2D.position = m_HitResults[0].point;
+            m_IsHanging = true;
+        }       
     }
 
     private void CheckForHanging()
     {
-        int hitCount = Physics2D.Raycast(transform.position + transform.up * (m_CapsuleCollider2D.size.y / 2 + m_CapsuleCollider2D.offset.y) * transform.localScale.y, 
-                                         transform.up, m_ContactFilter2D, m_HitResults, castDistance);
+        int hitCount = Physics2D.Raycast(transform.position + ((Vector3)m_CapsuleCollider2D.offset + Vector3.up * m_CapsuleCollider2D.size.y / 2) * transform.lossyScale.y, 
+                                        Vector3.up, m_HangingCheckContactFilter2D, m_HitResults, hangingCastDistance);
 
-        Debug.DrawRay(transform.position + transform.up * (m_CapsuleCollider2D.size.y/2 + m_CapsuleCollider2D.offset.y) * transform.localScale.y, transform.up * castDistance);
+        Debug.DrawRay(transform.position + ((Vector3)m_CapsuleCollider2D.offset + Vector3.up * m_CapsuleCollider2D.size.y / 2) * transform.lossyScale.y, 
+                        Vector3.up * hangingCastDistance);
 
         if (hitCount > 0)
         {
-            //transform.rotation = Quaternion.Slerp(transform.rotation, QuaternionExtension.RotateToDirection(m_HitResults[0].normal, -90), 5f * Time.deltaTime);
+            m_Rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
             m_Rigidbody2D.position = m_HitResults[0].point;
-            Debug.Log("Hit");
         }
         else
         {
-            //transform.rotation = Quaternion.Slerp(transform.rotation, QuaternionExtension.RotateToDirection(Vector3.down, 90), 5f * Time.deltaTime);
+            m_Rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+            m_IsHanging = false;
         }
 
     }
-
-
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, jumpCastRadius);
+        Gizmos.DrawWireSphere(transform.position + Vector3.one * jumpCastDistance/2, jumpCastRadius);
+        Gizmos.DrawWireSphere(transform.position + Vector3.one * jumpCastDistance, jumpCastRadius);
+    }
+#endif
 }
